41813cb81e2d0d124d0a0a37a21e4dc3bcfa969b
Pavel.Sergeev <pavel.sergeev@jetbrains.com>
2022-09-19 16:26:09 +0200

    make SVG parsing is locale-dependent
    skia issue: https://bugs.chromium.org/p/skia/issues/detail?id=11132

--- src/utils/SkParse.cpp
+++ src/utils/SkParse.cpp
@@ -213,12 +213,26 @@ const char* SkParse::FindMSec(const char str[], SkMSec* value)
     return str;
 }
 
+#include <locale.h>
+
+#if defined(SK_BUILD_FOR_MAC) || defined(SK_BUILD_FOR_IOS)
+#include <xlocale.h>
+#include <cstring>
+#endif
+
+#if defined(SK_BUILD_FOR_WIN)
+  static const _locale_t kDefaultLocale = _create_locale(LC_ALL, "C");
+  #define strtof_l(a, b, c) _strtof_l((a), (b), (c))
+#else
+  static const locale_t kDefaultLocale = newlocale(LC_ALL_MASK, "C", nullptr);
+#endif
+
 const char* SkParse::FindScalar(const char str[], SkScalar* value) {
     SkASSERT(str);
     str = skip_ws(str);
 
     char* stop;
-    float v = (float)strtod(str, &stop);
+    float v = strtof_l(str, &stop, kDefaultLocale);
     if (str == stop) {
         return nullptr;
     }
