826b28d041c4068d9218a4fa0cc05963ef6a6037
Ilya Ryzhenkov <orangy@jetbrains.com>
2023-05-17 16:13:22 +0300

    Add exclusion for iOS-specific code for tvOS

--- src/gpu/ganesh/mtl/GrMtlCaps.mm
+++ src/gpu/ganesh/mtl/GrMtlCaps.mm
@@ -96,7 +96,7 @@ bool GrMtlCaps::getGPUFamilyFromFeatureSet(id<MTLDevice> device,
             return true;
         }
     }
-#elif defined(SK_BUILD_FOR_IOS)
+#elif defined(SK_BUILD_FOR_IOS) || !defined(SK_BUILD_FOR_TVOS)
     // TODO: support tvOS
    *gpuFamily = GPUFamily::kApple;
     // iOS 12
